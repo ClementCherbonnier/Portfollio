cd9a9e4363e886cf7ef4ab9f166623b3f6f17d9f
Clement Cherbonnier <70938021+ClementCherbonnier@users.noreply.github.com>
2023-02-15 15:14:07 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+  <link rel="stylesheet" href="./views/Ressources/Css/portfolio.css">
+</head>
+
+<body>  
+  <section class="banner" id="home">
+    <div class="imgSidebar">
+      <img src="./views/Ressources/Images/img_perso.jpg" id="me">
+    </div>
+    <div class="contentBox">
+      <img class="imgBackground" id="Logo" src="./views/Ressources/Images/Logo_O-L.png">
+      <h5 class="logoText">Mon_Portfolio</h5>
+      <div>
+        <h4><span>Bonjour</span>, Je suis</h4>
+        <h2>CHERBONNIER Clément</h2>
+        <h4>Étudiant en licence professionnelle TQL</h4>
+        <p>J'ai 21 ans et je suis en licence professionnelle TQL (Test et Qualité Logiciel) dans le département Informatique de l'IUT de Laval. <br>
+          Je suis le créateur de ce portfolio ainsi que de Librarity qui est mon univers informatique.</p>
+        <a href="<?php echo BaseURL?>" class="btn">Librarity</a>
+      </div>
+      <ul class="sci">
+        <li><a href="https://www.linkedin.com/in/cl%C3%A9ment-cherbonnier/" target="_blank"><img src="./views/Ressources/Images/linkedin.png"></a></li>
+        <li><a href="https://github.com/ClementCherbonnier" target="_blank"><img src="./views/Ressources/Images/github.png"></a></li>
+      </ul>
+    </div>
+  </section>
+
+  <section class="about" id="about">
+    <div class="title white">
+      <h2>À propos</h2>
+      <p>Jeune programmeur Informatique de 20 ans</p>
+    </div>
+    <div class="content">
+      <div class="textBox">
+        <p>Je m’intéresse beaucoup au domaine de l’informatique car c’est un domaine qui permet de créer des projet ce qui se rapproche le plus de l'irréel ou du rêve.
+          On peut faire ce que l’on veut et lui donner "vie". Ce qui n’est pas le cas quand on créer une sculpture par exemple. C’est cette envie de créer, de modéliser un projet. Que dis-je
+          , un rêve ! Qui me motive à travailler dans la programmation, le développement informatique. <br>
+          Je suis aussi beaucoup intéressé par les jeux vidéos quand l’immersion dans son univers nous prend. J'aime aussi les série et film de science-fiction qui nous 
+          donnent envie de devenir nos héros favoris. Je porte aussi un intérêt pour l’animation 3D. Cet intérêt est devenu si fort que j’en fait un hobby afin de
+          modéliser des idées, des envie, des rêves. <br> <br>
+          
+          Je suis un rêveur. Mais quand je suis dans le monde réel je suis d’un naturel sérieux. J’observe ce qui m’entoure et m’en inspire. Je ne m’énerve pas 
+          facilement quand j’attends quelque chose. Je me trouve sympathique, j’essaie de poser le moins de soucis aux autres en m’adaptant à eux. Quand je commence 
+          quelque chose, il m’est difficile d’arrêter tant que je ne suis pas vraiment bloqué.
+          </p>
+      </div>
+      <div class="imgBox">
+        <img id="metwo" src="./views/Ressources/Images/IMG_2.jpg">
+      </div>
+    </div>
+  </section>
+
+  <section class="services" id="services">
+    <div class="title">
+      <h2>Mes services</h2>
+      <p>Savoir-faire, compétences, connaissances</p>
+    </div>
+    <div class="content">
+      <div class="servicesBox">
+        <img src="./views/Ressources/Images/icon1.png">
+        <h2>Web Design</h2>
+        <p>J'ai beaucoup appris en suivant des tutos et en fesant, dans le cadre d'un projet de ma formation, de la
+          programmation web côté serveur qui m'ont permis d'employé les langages html, css, php et javascript.</p>
+      </div>
+      <div class="servicesBox">
+        <img src="./views/Ressources/Images/icon3.png">
+        <h2>Android</h2>
+        <p>J'ai créé en l'espace de 2 mois (temps de travail réel : 6 jours) un petit jeu mobile dans le style de Pong.
+          J'ai aussi créé avec une équipes un projet de jeu de cartes qui a pris 5 mois de développement.</p>
+      </div>
+      <div class="servicesBox">
+        <img src="./views/Ressources/Images/icon6.png">
+        <h2>Animation</h2>
+        <p>J'ai développé certaine compétence pour l'animation 3D grâce à un passe temps en utilisant cinéma 4D ainsi
+          qu'un peut de 2D en fesant un mini projet Unity</p>
+      </div>
+    </div>
+  </section>
+
+  <section class="work" id="work">
+    <div class="title">
+      <h2>Projets effectuées</h2>
+      <p>Vous verrez la listes de différent projets que j'ai effectuer sans les détails</p>
+    </div>
+    <div class="content">
+      <div class="workBox">
+        <div class="imgBox">
+          <img src="./views/Ressources/Images/project_stage.png">
+        </div>
+        <div class="textBox">
+          <h3>Web</h3>
+          <p>Stage : gestion des tickets <br> 2021</p>
+        </div>
+      </div>
+      <div class="workBox">
+        <div class="imgBox">
+          <img src="./views/Ressources/Images/project_pts3.jpg">
+        </div>
+        <div class="textBox">
+          <h3>Android</h3>
+          <p>projet tutoré du semestre 3 <br> 2020-2021</p>
+        </div>
+      </div>
+      <div class="workBox">
+        <div class="imgBox">
+          <img src="./views/Ressources/Images/projet_Doombringer.jpg">
+        </div>
+        <div class="textBox">
+          <h3>Animation 3D</h3>
+          <p>Doombringer<br> 2020</p>
+        </div>
+      </div>
+      <div class="workBox">
+        <div class="imgBox">
+          <img src="./views/Ressources/Images/project_eCommerce.png">
+        </div>
+        <div class="textBox">
+          <h3>Framework</h3>
+          <p>Site e-commerce avec symphony <br> 2021</p>
+        </div>
+      </div>
+      <div class="workBox">
+        <div class="imgBox">
+          <img src="./views/Ressources/Images/project_Unity.png">
+        </div>
+        <div class="textBox">
+          <h3>Unity</h3>
+          <p>Plateforme de jeux vidéo <br> 2021</p>
+        </div>
+      </div>
+      <!-- <div class="workBox">
+        <div class="imgBox">
+          <img src="images/img6.jpg">
+        </div>
+        <div class="textBox">
+          <h3>Projet 06</h3>
+        </div>
+      </div>
+      <div class="workBox">
+        <div class="imgBox">
+          <img src="images/img7.jpg">
+        </div>
+        <div class="textBox">
+          <h3>Projet 07</h3>
+        </div>
+      </div>
+      <div class="workBox">
+        <div class="imgBox">
+          <img src="images/img8.jpg">
+        </div>
+        <div class="textBox">
+          <h3>Projet 08</h3>
+        </div>
+      </div> -->
+    </div>
+  </section>
+
+  <section class="contact" id="contact">
+    <div class="title white">
+      <h2>Me Contacter</h2>
+      <p>Vous pouvez m'envoyer un mail via l'adresse clemt72@gmail.com ou en remplissant le formulaire ci-dessous <br> <br>
+      Cependant, sans php, l'envoie d'un message grâce à ce formulaire ne s'effectueras pas</p>
+    </div>
+    <div class="contactForm">
+      <div class="row">
+        <div class="col50">
+          <input type="text" name="" placeholder="Prénom">
+        </div>
+        <div class="col50">
+          <input type="text" name="" placeholder="Nom">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col50">
+          <input type="text" name="" placeholder="Email">
+        </div>
+        <div class="col50">
+          <input type="text" name="" placeholder="N° de téléphone">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col100">
+          <textarea placeholder="Message"></textarea>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col100">
+          <input type="submit" value="Send">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <script type="text/javascript">
+    let Logo = document.getElementById("Logo");
+    let me = document.getElementById("me");
+    let metwo = document.getElementById("metwo");
+
+    window.addEventListener('scroll', function () {
+      var value = window.scrollY;
+
+      Logo.style.top = value * 0.5 + 'px';
+      me.style.left = value * -0.5 + 'px';
+    })
+  </script>
+</body>
+
+</html>
